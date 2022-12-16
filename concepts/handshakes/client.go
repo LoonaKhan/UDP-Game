d747b80a0891f20acea49c4e75b1368d68676b6b
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-12-16 02:44:38 -0500

    researched handshakes and have a working demo
    wont be too hard to implement

---
+++ concepts/handshakes/client.go
@@ -0,0 +1,45 @@
+package main
+
+/**
+tests handshakes as well as multiple clients
+
+connects to the server along with other clients.
+the server keeps track of and responds to all of them at the same time.
+test this with 2 seperate clients running a few seconds out of sync
+
+todo: how to keep track of order when Reading?
+	continue to use headers? seperate goroutine?
+
+*/
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	s, err := net.ResolveUDPAddr("udp4", "localhost:4001")
+	c, err := net.DialUDP("udp4", nil, s)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("connected to server")
+	defer c.Close()
+
+	buffer := make([]byte, 1024)
+	for {
+		fmt.Println()
+		time.Sleep(time.Second * 10)
+
+		_, err = c.Write([]byte("hello"))
+
+		_, _, err = c.ReadFromUDP(buffer)
+		fmt.Println(string(buffer))
+
+		_, _, _ = c.ReadFromUDP(buffer)
+		fmt.Println(string(buffer))
+	}
+}
---
+++ concepts/handshakes/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+var addys = map[string]*net.UDPAddr{} // all clients are stored here
+// todo: remove disconnected ones?
+// 	goroutine that continuously check them? or when we respond to anyone, we dial them first.
+
+func main() {
+	port := ":4001"
+
+	s, err := net.ResolveUDPAddr("udp4", port)
+	conn, err := net.ListenUDP("udp4", s)
+	defer conn.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Listening on Port: %s\n", port)
+
+	buffer := make([]byte, 1024)
+	for {
+		_, addr, err := conn.ReadFromUDP(buffer)
+		_, exists := addys[fmt.Sprintf("%s:%d", addr.IP.String(), addr.Port)]
+		if !exists {
+			addys[fmt.Sprintf("%s:%d", addr.IP.String(), addr.Port)] = addr
+		}
+		fmt.Printf("addys: %d\n", len(addys))
+		fmt.Println(addys)
+
+		go func() {
+			_, err = net.DialUDP("udp4", nil, addr) // check if the client exists
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				fmt.Printf("connected back to client: %s:%d\n", addr.IP.String(), addr.Port)
+
+				for a := range addys { // sends to all clients
+					_, err = conn.WriteToUDP([]byte("Hey!"+time.Now().String()), addys[a])
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			}
+		}()
+
+	}
+}
