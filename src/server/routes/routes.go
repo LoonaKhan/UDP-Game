3cc27a1e7d221f8e5ca579416f20771824c9928d
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-12-26 19:35:49 -0500

    Update routes.go

--- src/server/routes/routes.go
+++ src/server/routes/routes.go
@@ -11,18 +11,9 @@ import (
 	"server/db/models/players"
 	"server/err_handling"
 	"server/routes/route_structs"
+	"server/utils/net_utils"
 	"time"
 )
-
-func FormatRes[T any](resData T, header string) []byte {
-	/*
-		marshals the sent data to json
-		appends the prefix to the data
-	*/
-	res, _ := json.Marshal(resData)
-	stringified := fmt.Sprintf("%s:%s", header, string(res))
-	return []byte(stringified)
-}
 
 var Methods = map[string]func(buffer []byte, conn *net.UDPConn, addr *net.UDPAddr){
 	"get_chunks:": func(buffer []byte, conn *net.UDPConn, addr *net.UDPAddr) {
@@ -192,7 +183,7 @@ var Methods = map[string]func(buffer []byte, conn *net.UDPConn, addr *net.UDPAdd
 		// if the player and addy each are not mapped
 		// map em together
 		a.Insert(plr, addr)
-		res := FormatRes(route_structs.PlayerID{Id: plr.ID}, "login")
+		res := net_utils.FormatRes(route_structs.PlayerID{Id: plr.ID}, "login")
 		conn.WriteToUDP(res, addr)
 	},
 
