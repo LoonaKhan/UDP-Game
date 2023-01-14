4b9c4de55c8b038816a148a59fdacdb4968368fb
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2023-01-14 17:30:27 -0500

    implemented BlockSprite

--- src/client/models/block/BlockSprite.cpp
+++ src/client/models/block/BlockSprite.cpp
@@ -8,7 +8,15 @@ namespace block {
 
     BlockSprite::BlockSprite(block::Block block)
     : block(block) {
+        auto pos = this->block.getCoords();
+        auto c = colours[this->block.getColour()];
 
+        this->setPosition((float)pos[0], (float)pos[1]);
+        this->setColor(sf::Color(c[0], c[1], c[2]));
+    }
+
+    void BlockSprite::render(sf::RenderWindow *window) {
+        window->draw(*this);
     }
 
 } // block
--- src/client/models/block/BlockSprite.h
+++ src/client/models/block/BlockSprite.h
@@ -17,12 +17,22 @@ namespace block {
 
     private: // attributes
         Block block;
+        sf::RectangleShape shape;
 
     public: // methods
         BlockSprite(Block block);
 
         void render(sf::RenderWindow *window);
     private: // methods
+    };
+
+    // the colour codes of each biome
+    unsigned char colours[5][3]{
+            {28, 43, 140},
+            {247, 250, 155},
+            {32, 110, 33},
+            {8, 87, 9},
+            {93, 97, 93}
     };
 
 } // block
